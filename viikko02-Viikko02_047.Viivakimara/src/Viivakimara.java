1e75e47503aeeb82f86a4d5d395c7fd3630708e6
Eetu Kurkinen <37667727+Eetuj@users.noreply.github.com>
2018-04-29 01:44:40 +0300

    Add files via upload

--- viikko02-Viikko02_047.Viivakimara/src/Viivakimara.java
+++ viikko02-Viikko02_047.Viivakimara/src/Viivakimara.java
@@ -4,12 +4,80 @@ import robotti.Ohjain;
 public class Viivakimara {
 
     public static void main(String[] args) {
+
         Ohjain.kaynnista();
 
         // testaa metodejasi täällä
+        viivakimara(5);
 
         Ohjain.sammuta();
+
     }
 
-    // toteuta metodisi tänne
+    public static void liikuMonta(int maara) {
+
+        int i = 0;
+
+        while (maara > i) {
+
+            Ohjain.liiku();
+
+            i++;
+
+        }
+
+    }
+
+    public static void piirraNelio(int sivu) {
+
+        liikuMonta(sivu);
+
+        Ohjain.oikea();
+
+        liikuMonta(sivu);
+
+        Ohjain.oikea();
+
+        liikuMonta(sivu);
+
+        Ohjain.oikea();
+
+        liikuMonta(sivu);
+
+        Ohjain.oikea();
+
+    }
+
+    public static void sisakkaisetNeliot(int montako) {
+
+        rajatutSisakkaisetNeliot(montako, 1);
+
+    }
+
+    public static void rajatutSisakkaisetNeliot(int suurin, int pienin) {
+
+        for (int i = pienin; i <= suurin; i++) {
+
+            piirraNelio(i);
+
+        }
+
+    }
+
+    public static void viivakimara(int koko) {
+
+        rajatutSisakkaisetNeliot(koko, koko - 2);
+
+        liikuMonta(koko);
+
+        Ohjain.oikea();
+
+        liikuMonta(koko);
+
+        Ohjain.oikea();
+
+        rajatutSisakkaisetNeliot(koko - 1, koko - 2);
+
+    }
+
 }
